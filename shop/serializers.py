bcc0259ef83faa5cfe21f1804382284c7c82edca
zakhep66 <sur.mikhael@gmail.com>
2023-09-27 14:34:27 +0300

    update s.Serializer into s.ModelSerializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -3,31 +3,31 @@ from rest_framework import serializers as s
 from .models import Product, Value, Category, Order, Attribute
 
 
-class ProductSerializer(s.Serializer):
+class ProductSerializer(s.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
 
-class ValueSerializer(s.Serializer):
+class ValueSerializer(s.ModelSerializer):
     class Meta:
         model = Value
         fields = '__all__'
 
 
-class CategorySerializer(s.Serializer):
+class CategorySerializer(s.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
 
-class OrderSerializer(s.Serializer):
+class OrderSerializer(s.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
 
-class AttributeSerializer(s.Serializer):
+class AttributeSerializer(s.ModelSerializer):
     class Meta:
         model = Attribute
         fields = '__all__'
